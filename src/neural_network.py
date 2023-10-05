15bd066803c2c52cca9a4ddef7ad25fdd9291295
Mehdi Mhb <mehdi.mohebbi23@gmail.com>
2023-10-05 15:58:36 +0330

    Update neural network

--- src/neural_network.py
+++ src/neural_network.py
@@ -4,11 +4,6 @@ from function_utils import sigmoid, relu, sigmoid_backward, relu_backward
 import h5py
 import logging
 
-logging.basicConfig(
-    filename='file2.log', filemode='w', level=logging.DEBUG,
-    format='%(asctime)s - %(message)s', datefmt='%H:%M:%S'
-)
-
 
 class Layer:
     def __init__(self, id: int, no_of_units: int, no_of_units_of_previous_layer: int, act_fun_forward, act_fun_backward) -> None:
@@ -28,7 +23,7 @@ class Layer:
         self.act_fun_backward = act_fun_backward
 
     def __repr__(self) -> str:
-        return f"{self.id}:{self.no_of_units}"
+        return f"{self.id}:{self.no_of_units}:{self.W.shape}"
 
 
 class Layers:
@@ -60,78 +55,79 @@ class Layers:
 class NeuralNetwork:
     def __init__(
         self,
-        training_set: NDArray,
-        training_set_labels: NDArray,
-        layers_dimension: ArrayLike,
+        layers_dimension: list,
+        training_set: NDArray = None,
+        training_set_labels: NDArray = None,
         learning_rate: float = 0.0075,
-        threshold: float = 0.5
     ) -> None:
         self.tX = training_set
         self.tY = training_set_labels
         self.learning_rate = learning_rate
-        self.threshold = threshold
 
         self.iteration = 0
-        self.features, self.size_of_training_set = self.tX.shape
         self.layers = self.initialize_layers(layers_dimension)
         self.costs = []
 
     def initialize_layers(self, layers_dimension: ArrayLike) -> Layers:
         layers = Layers()
-        for layer in range(len(layers_dimension)):
-            if layer == 0:
-                layers.add(Layer(layer+1, layers_dimension[layer], self.features, relu, relu_backward))
-            elif layer == len(layers_dimension)-1:
+        for layer in range(1, len(layers_dimension)):
+            if layer == len(layers_dimension)-1:
                 layers.add(Layer(layer+1, layers_dimension[layer], layers_dimension[layer-1], sigmoid, sigmoid_backward))
             else:
                 layers.add(Layer(layer+1, layers_dimension[layer], layers_dimension[layer-1], relu, relu_backward))
         return layers
 
-    def forward(self):
+    def forward(self, data: NDArray) -> None:
         layer = self.layers.first
         while layer:
             if layer is self.layers.first:
-                layer.Z = np.dot(layer.W, self.tX) + layer.b
+                layer.Z = np.dot(layer.W, data) + layer.b
             else:
                 layer.Z = np.dot(layer.W, layer.prev.A) + layer.b
             layer.A = layer.act_fun_forward(layer.Z)
             layer = layer.next
 
-    def compute_cost(self):
-        cost = -(np.dot(self.tY, np.log(self.layers.last.A).T) + np.dot(1-self.tY, np.log(1-self.layers.last.A).T))/self.size_of_training_set
+    def compute_cost(self, labels: NDArray) -> float:
+        size = labels.shape[1]
+        cost = -(np.dot(labels, np.log(self.layers.last.A).T) + np.dot(1-labels, np.log(1-self.layers.last.A).T))/size
         return np.squeeze(cost)
 
-    def backward(self):
+    def backward(self, labels: NDArray) -> None:
+        size = labels.shape[1]
         layer = self.layers.last
         while layer:
             if layer is self.layers.last:
-                layer.dA = - (np.divide(self.tY, layer.A) - np.divide(1 - self.tY, 1 - layer.A))
+                layer.dA = - (np.divide(labels, layer.A) - np.divide(1 - labels, 1 - layer.A))
             layer.dZ = layer.act_fun_backward(layer.dA, layer.Z)
-            layer.db = np.sum(layer.dZ, axis=1, keepdims=True)/self.size_of_training_set
+            layer.db = np.sum(layer.dZ, axis=1, keepdims=True)/size
             if layer is self.layers.first:
-                layer.dW = np.dot(layer.dZ, self.tX.T)/self.size_of_training_set
+                layer.dW = np.dot(layer.dZ, self.tX.T)/size
             else:
-                layer.dW = np.dot(layer.dZ, layer.prev.A.T)/self.size_of_training_set
+                layer.dW = np.dot(layer.dZ, layer.prev.A.T)/size
                 layer.prev.dA = np.dot(layer.W.T, layer.dZ)
             layer = layer.prev
 
-    def update_layers(self):
+    def update_layers(self) -> None:
         layer = self.layers.first
         while layer:
             layer.W = layer.W - self.learning_rate * layer.dW
             layer.b = layer.b - self.learning_rate * layer.db
             layer = layer.next
 
-    def train(self, no_of_iterations):
+    def train(self, no_of_iterations, training_set: NDArray = None, training_set_labels: NDArray = None) -> None:
         for i in range(no_of_iterations):
-            self.forward()
-            self.backward()
+            self.forward(self.tX if training_set is None else training_set)
+            self.backward(self.tY if training_set_labels is None else training_set_labels)
             self.update_layers()
             self.iteration += 1
             if self.iteration % 100 == 0 or i == no_of_iterations-1:
-                cost = self.compute_cost()
+                cost = self.compute_cost(self.tY if training_set is None else training_set_labels)
                 logging.debug(f"Cost after iteration {self.iteration}: {cost}")
                 self.costs.append(cost)
+
+    def evaluate(self, data: NDArray):
+        self.forward(data)
+        return np.squeeze(self.layers.last.A)
 
 
 def load_dataset():
@@ -153,7 +149,8 @@ def load_dataset():
 
 if __name__ == "__main__":
     training_set, training_set_labels, test_set, test_set_labels = load_dataset()
-    nn = NeuralNetwork(training_set, training_set_labels, [20, 7, 5, 1])
+    logging.debug(training_set.shape)
+    nn = NeuralNetwork([12288, 7, 5, 1], training_set, training_set_labels)
     nn.train(1000)
     # logging.debug(nn.costs)
     nn.train(1000)
