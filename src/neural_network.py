d91b972ba7b236dae47067f428f93b7b973ab0ce
Mehdi Mhb <mehdi.mohebbi23@gmail.com>
2023-10-27 01:10:22 +0330

    Add regularization and dropout

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,26 +1,71 @@
 import numpy as np
 from numpy.typing import NDArray, ArrayLike
+from typing import Callable
 from function_utils import sigmoid, relu, sigmoid_backward, relu_backward
 import h5py
 import logging
+from src.confusion_matrix import ConfusionMatrix
+
+logging.basicConfig(
+    filename='file.log', filemode='w', level=logging.DEBUG,
+    format='%(message)s'
+)
 
 
 class Layer:
-    def __init__(self, id: int, no_of_units: int, no_of_units_of_previous_layer: int, act_fun_forward, act_fun_backward) -> None:
+    def __init__(
+        self,
+        id: int,
+        no_of_units: int,
+        no_of_units_of_previous_layer: int,
+        weight_initialization_method: str,
+        activation_function: str,
+        keeping_neuron_probability_in_dropout: float
+    ) -> None:
         self.id = id
         self.next: Layer = None
         self.prev: Layer = None
         self.no_of_units = no_of_units
-        self.W = np.random.randn(no_of_units, no_of_units_of_previous_layer)/np.sqrt(no_of_units_of_previous_layer)
+        self.W = self.weight_initialization(weight_initialization_method, no_of_units, no_of_units_of_previous_layer)
         self.dW: NDArray
         self.b = np.zeros((no_of_units, 1))
         self.db: NDArray
-        self.A: NDArray
-        self.dA: NDArray
         self.Z: NDArray
         self.dZ: NDArray
-        self.act_fun_forward = act_fun_forward
-        self.act_fun_backward = act_fun_backward
+        self.A: NDArray
+        self.dA: NDArray
+        self._dropout: NDArray
+        self.keep_probability = keeping_neuron_probability_in_dropout
+        self.act_fun_forward, self.act_fun_backward = self.function_detection(activation_function)
+
+    def dropout(self, A: NDArray, forward: bool = False) -> NDArray:
+        if forward:
+            self._dropout = np.random.rand(self.A.shape[0], self.A.shape[1])
+            self._dropout = (self._dropout < self.keep_probability).astype(int)
+        A *= self._dropout
+        A /= self.keep_probability
+        return A
+
+    def weight_initialization(
+        self,
+        weight_initialization_method: str,
+        no_of_units: int,
+        no_of_units_of_previous_layer: int = 0
+    ):
+        match weight_initialization_method:
+            case 'zero':
+                return np.zeros((no_of_units, no_of_units_of_previous_layer))
+            case 'he':
+                return np.random.randn(no_of_units, no_of_units_of_previous_layer)*np.sqrt(2/no_of_units_of_previous_layer)
+            case 'xavier':
+                return np.random.randn(no_of_units, no_of_units_of_previous_layer)*np.sqrt(2/(no_of_units+no_of_units_of_previous_layer))
+
+    def function_detection(self, f: str) -> tuple[Callable[[NDArray], NDArray], Callable[[NDArray, NDArray], NDArray]]:
+        match f:
+            case 'sigmoid':
+                return sigmoid, sigmoid_backward
+            case 'relu':
+                return relu, relu_backward
 
     def __repr__(self) -> str:
         return f"{self.id}:{self.no_of_units}:{self.W.shape}"
@@ -32,15 +77,28 @@ class Layers:
         self.last: Layer = None
 
     def __repr__(self) -> str:
-        layer = self.first
         r = ""
-        while layer:
+        for layer in self:
             if layer is self.last:
                 r += str(layer)
             else:
                 r += str(layer) + " -> "
-            layer = layer.next
         return r
+
+    def __len__(self):
+        return np.sum([1 for _ in self])
+
+    def __iter__(self):
+        layer = self.first
+        while layer:
+            yield layer
+            layer = layer.next
+
+    def __reversed__(self):
+        layer = self.last
+        while layer:
+            yield layer
+            layer = layer.prev
 
     def add(self, new_layer: Layer) -> None:
         if self.first is None:
@@ -55,7 +113,7 @@ class Layers:
 class NeuralNetwork:
     def __init__(
         self,
-        layers_dimension: list,
+        layers_description: list[tuple[int, None, None] | tuple[int, str, str]],
         training_set: NDArray = None,
         training_set_labels: NDArray = None,
         learning_rate: float = 0.0075,
@@ -65,68 +123,86 @@ class NeuralNetwork:
         self.learning_rate = learning_rate
 
         self.iteration = 0
-        self.layers = self.initialize_layers(layers_dimension)
+        self.layers = self.build_layers(layers_description)
         self.costs = []
 
-    def initialize_layers(self, layers_dimension: ArrayLike) -> Layers:
+    def build_layers(self, layers_description: list[tuple[int, None, None] | tuple[int, str, str]]) -> Layers:
         layers = Layers()
-        for layer in range(1, len(layers_dimension)):
-            if layer == len(layers_dimension)-1:
-                layers.add(Layer(layer+1, layers_dimension[layer], layers_dimension[layer-1], sigmoid, sigmoid_backward))
-            else:
-                layers.add(Layer(layer+1, layers_dimension[layer], layers_dimension[layer-1], relu, relu_backward))
+        for layer in range(1, len(layers_description)):
+            layers.add(Layer(
+                layer+1,
+                layers_description[layer][0],
+                layers_description[layer-1][0],
+                layers_description[layer][1],
+                layers_description[layer][2],
+                layers_description[layer][3]
+            ))
         return layers
 
-    def forward(self, data: NDArray) -> None:
-        layer = self.layers.first
-        while layer:
+    def forward(self, data: NDArray, dropout: bool) -> None:
+        for layer in self.layers:
             if layer is self.layers.first:
                 layer.Z = np.dot(layer.W, data) + layer.b
             else:
                 layer.Z = np.dot(layer.W, layer.prev.A) + layer.b
             layer.A = layer.act_fun_forward(layer.Z)
-            layer = layer.next
+            if dropout:
+                layer.A = layer.dropout(layer.A, forward=True)
 
-    def compute_cost(self, labels: NDArray) -> float:
+    def backward(self, labels: NDArray, reg_lambd: float, dropout: bool) -> None:
         size = labels.shape[1]
-        cost = -(np.dot(labels, np.log(self.layers.last.A).T) + np.dot(1-labels, np.log(1-self.layers.last.A).T))/size
-        return np.squeeze(cost)
-
-    def backward(self, labels: NDArray) -> None:
-        size = labels.shape[1]
-        layer = self.layers.last
-        while layer:
+        for layer in reversed(self.layers):
             if layer is self.layers.last:
                 layer.dA = - (np.divide(labels, layer.A) - np.divide(1 - labels, 1 - layer.A))
             layer.dZ = layer.act_fun_backward(layer.dA, layer.Z)
             layer.db = np.sum(layer.dZ, axis=1, keepdims=True)/size
             if layer is self.layers.first:
-                layer.dW = np.dot(layer.dZ, self.tX.T)/size
+                layer.dW = np.dot(layer.dZ, self.tX.T)/size + reg_lambd*layer.W/size
             else:
-                layer.dW = np.dot(layer.dZ, layer.prev.A.T)/size
+                layer.dW = np.dot(layer.dZ, layer.prev.A.T)/size + reg_lambd*layer.W/size
                 layer.prev.dA = np.dot(layer.W.T, layer.dZ)
-            layer = layer.prev
+                if dropout:
+                    layer.prev.dA = layer.prev.dropout(layer.prev.dA)
 
     def update_layers(self) -> None:
-        layer = self.layers.first
-        while layer:
+        for layer in self.layers:
             layer.W = layer.W - self.learning_rate * layer.dW
             layer.b = layer.b - self.learning_rate * layer.db
-            layer = layer.next
 
-    def train(self, no_of_iterations, training_set: NDArray = None, training_set_labels: NDArray = None) -> None:
+    def compute_cost(self, labels: NDArray, regularization: str = None, reg_lambd: float = 0) -> float:
+        size = labels.shape[1]
+        cost = -(np.dot(labels, np.log(self.layers.last.A).T) + np.dot(1-labels, np.log(1-self.layers.last.A).T))/size
+        if regularization is not None:
+            regularization_cost = 0
+            for layer in self.layers:
+                if regularization == 'L1':
+                    regularization_cost += np.sum(np.abs(layer.W))
+                elif regularization == 'L2':
+                    regularization_cost += np.sum(np.square(layer.W))
+            cost += regularization_cost*reg_lambd/(2*size)
+        return np.squeeze(cost)
+
+    def train(
+        self,
+        no_of_iterations,
+        training_set: NDArray = None,
+        training_set_labels: NDArray = None,
+        regularization: str = None,
+        reg_lambd: float = 0,
+        dropout: bool = False
+    ) -> None:
         for i in range(no_of_iterations):
-            self.forward(self.tX if training_set is None else training_set)
-            self.backward(self.tY if training_set_labels is None else training_set_labels)
+            self.forward(self.tX if training_set is None else training_set, dropout)
+            self.backward(self.tY if training_set_labels is None else training_set_labels, reg_lambd, dropout)
             self.update_layers()
             self.iteration += 1
             if self.iteration % 100 == 0 or i == no_of_iterations-1:
-                cost = self.compute_cost(self.tY if training_set is None else training_set_labels)
+                cost = self.compute_cost(self.tY if training_set is None else training_set_labels, regularization, reg_lambd)
                 logging.debug(f"Cost after iteration {self.iteration}: {cost}")
                 self.costs.append(cost)
 
-    def evaluate(self, data: NDArray):
-        self.forward(data)
+    def predict(self, data: NDArray):
+        self.forward(data, False)
         return np.squeeze(self.layers.last.A)
 
 
@@ -149,8 +225,15 @@ def load_dataset():
 
 if __name__ == "__main__":
     training_set, training_set_labels, test_set, test_set_labels = load_dataset()
-    logging.debug(training_set.shape)
-    nn = NeuralNetwork([12288, 7, 5, 1], training_set, training_set_labels)
-    nn.train(1000)
-    # logging.debug(nn.costs)
-    nn.train(1000)
+    layer = [
+        (12288, None, None, None),
+        (7, 'he', 'relu', 0.8),
+        (5, 'he', 'relu', 0.7),
+        (1, 'he', 'sigmoid', 1)
+    ]
+    model = NeuralNetwork(layer, training_set, training_set_labels)
+    model.train(2000, dropout=True)
+    logging.debug(model.costs)
+    cm = ConfusionMatrix(model, test_set, test_set_labels)
+    logging.debug(cm)
+    logging.debug(cm.statistics())
